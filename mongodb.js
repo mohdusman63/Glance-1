5faa2be3cd1565887aa42293c59f664f6b28dfdf
sfsusman <79912976+sfsusman@users.noreply.github.com>
2021-03-15 21:38:20 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,11 @@
 
+ db.demo396.aggregate([
+    { "$lookup": {
+       "from": "demo395",
+       "let": { "details": "$details" },
+       "pipeline": [
+          { "$match": { "$expr": { "$in": [ "$_id", "$$details" ] } } }
+       ],
+       "as": "output"
+    }}
+ ])
