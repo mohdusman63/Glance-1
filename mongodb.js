4ace3d95918997218eac8bc69854026683ec2ed1
sfsusman <79912976+sfsusman@users.noreply.github.com>
2021-03-19 16:46:00 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -16,3 +16,46 @@
        {lookup:{from :" ",locaiField:"product" ,foreignField:"",as:""}}
     }}
  ])
+
+
+
+
+
+######################################glance###########################################
+  1-->db.getCollection('services').aggregate([
+            {
+                "$lookup": {
+                    from: "categories",
+                    localField: "categoryId",
+                    foreignField: "_id",
+                    as: "categoryDetails"
+                }},
+             {
+                 $match: matchVal
+             },
+             {
+                 $sort: {
+                     [sortField]: sortOrder
+                 }            },
+            {
+                $group: {
+                    _id: null,
+                    total: {
+                        $sum: 1              //count the total document 
+                    },
+                    results: {
+                        $push: '$$ROOT'
+                    }
+                }
+            },
+            {
+                $project: {
+                    total: 1,
+                    results: {
+                        // [previous_result, skip_page,limit]  (limit * (page - 1)), limit
+                        $slice: ['$results', (limit * (page - 1)), limit]
+                    }
+                }
+            },
+        ])
+		
